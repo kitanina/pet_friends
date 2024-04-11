e7c72370d0695ffebbf1e1462f10b60f77971091
Elena Kitanina <kitaninaelena@gmail.com>
2024-04-11 12:24:42 +0300

    test

--- tests/frontend/test_ui_reg_user.py
+++ tests/frontend/test_ui_reg_user.py
@@ -1,9 +1,24 @@
-def test_reg_user(main_page, user_data):
+def test_reg_user(user_page, user_data):
     name = user_data['name']
     email = user_data['email']
     password = user_data['password']
-    new_user_page = main_page.get_new_user_page()
-    main_page = new_user_page.get_main_page(name, email, password)
-    assert main_page.card_deck_grid.is_displayed()
+    new_user_page = user_page.get_new_user_page()
+    user_page = new_user_page.get_user_page(name, email, password)
+    assert user_page.card_deck_grid.is_displayed()
+
+
+
+def test_exit_from_user_page(open_user_page):
+    user_page, user_date = open_user_page
+    assert user_page.card_deck_grid.is_displayed()
+
+    sign_up_page = user_page.get_sign_up_page()
+    assert sign_up_page.registration_button.is_displayed()
+
+
+
+
+
+
 
 
--- ui_framework/pages/main_page.py
+++
@@ -1,9 +0,0 @@
-from selenium.webdriver.common.by import By
-from ui_framework.pages.base_page import BasePage
-
-
-class MainPage(BasePage):
-    """ Class for Main page. """
-    @property
-    def card_deck_grid(self):
-        return self.driver.find_element(By.XPATH, '//div[@class="card-deck"]')
